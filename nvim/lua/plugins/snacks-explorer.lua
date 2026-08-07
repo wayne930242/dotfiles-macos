c6a5bc5467ec3fc9d41456f6c3c934c1f15eee89
Wei Hung <wayne930242@gmail.com>
2026-08-07 15:16:22 +0800

    nvim: 放行常見密碼/密鑰檔案通過 snacks explorer 的 gitignore 過濾
    擴充 explorer.include,除了 .env* 之外也放行 .vault_pass、
    .secrets/**、.token-override、*.pem、*.key,避免這類被
    .gitignore 排除的敏感檔案在側欄完全看不到。

--- nvim/lua/plugins/snacks-explorer.lua
+++ nvim/lua/plugins/snacks-explorer.lua
@@ -6,8 +6,8 @@ return {
         explorer = {
           hidden = true,
           -- include 優先於 hidden/ignored/exclude,用來放行被 .gitignore
-          -- 擋掉的 .env*,又不必開 ignored 讓 node_modules 灌進側欄
-          include = { ".env*" },
+          -- 擋掉的密碼/密鑰類檔案,又不必開 ignored 讓 node_modules 灌進側欄
+          include = { ".env*", ".vault_pass", ".secrets/**", ".token-override", "*.pem", "*.key" },
           actions = {
             explorer_yazi = function(_, item)
               if item then
